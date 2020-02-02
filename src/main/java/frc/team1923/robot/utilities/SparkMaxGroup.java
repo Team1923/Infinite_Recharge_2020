273dc1fe99be7c793163804f4852424510d03775
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-02-02 10:11:17 -0500

    Add motor controller groups

---
+++ src/main/java/frc/team1923/robot/utilities/SparkMaxGroup.java
@@ -0,0 +1,48 @@
+package frc.team1923.robot.utilities;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class SparkMaxGroup {
+    private final int leaderID;
+    private final int[] followerIDs;
+
+    private final boolean inverted;
+    private final boolean coast;
+
+    private SparkMaxGroup(int leaderID, int[] followerIDs, boolean inverted, boolean coast) {
+        this.leaderID = leaderID;
+        this.followerIDs = followerIDs;
+        this.inverted = inverted;
+        this.coast = coast;
+    }
+
+    public SparkMaxGroup(int leaderID, int... followerIDs) {
+        this(leaderID, followerIDs, false, false);
+    }
+
+    public SparkMaxGroup inverting() {
+        return new SparkMaxGroup(this.leaderID, this.followerIDs, true, this.coast);
+    }
+
+    public SparkMaxGroup coasting() {
+        return new SparkMaxGroup(this.leaderID, this.followerIDs, this.inverted, true);
+    }
+
+    public CANSparkMax create() {
+        CANSparkMax leader = new CANSparkMax(this.leaderID, MotorType.kBrushless);
+        leader.restoreFactoryDefaults();
+        leader.setInverted(this.inverted);
+        leader.setIdleMode(this.coast ? IdleMode.kCoast : IdleMode.kBrake);
+
+        for (int followerID : this.followerIDs) {
+            CANSparkMax follower = new CANSparkMax(followerID, MotorType.kBrushless);
+            follower.restoreFactoryDefaults();
+            follower.setIdleMode(this.coast ? IdleMode.kCoast : IdleMode.kBrake);
+            follower.follow(leader);
+        }
+
+        return leader;
+    }
+}
---
+++ src/main/java/frc/team1923/robot/utilities/TalonFXGroup.java
@@ -0,0 +1,48 @@
+package frc.team1923.robot.utilities;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class TalonFXGroup {
+    private final int leaderID;
+    private final int[] followerIDs;
+
+    private final boolean inverted;
+    private final boolean coast;
+
+    private TalonFXGroup(int leaderID, int[] followerIDs, boolean inverted, boolean coast) {
+        this.leaderID = leaderID;
+        this.followerIDs = followerIDs;
+        this.inverted = inverted;
+        this.coast = coast;
+    }
+
+    public TalonFXGroup(int leaderID, int... followerIDs) {
+        this(leaderID, followerIDs, false, false);
+    }
+
+    public TalonFXGroup inverting() {
+        return new TalonFXGroup(this.leaderID, this.followerIDs, true, this.coast);
+    }
+
+    public TalonFXGroup coasting() {
+        return new TalonFXGroup(this.leaderID, this.followerIDs, this.inverted, true);
+    }
+
+    public WPI_TalonFX create() {
+        WPI_TalonFX leader = new WPI_TalonFX(this.leaderID);
+        leader.configFactoryDefault();
+        leader.setInverted(this.inverted);
+        leader.setNeutralMode(this.coast ? NeutralMode.Coast : NeutralMode.Brake);
+
+        for (int followerID : this.followerIDs) {
+            WPI_TalonFX follower = new WPI_TalonFX(followerID);
+            follower.configFactoryDefault();
+            follower.setInverted(this.inverted);
+            follower.setNeutralMode(this.coast ? NeutralMode.Coast : NeutralMode.Brake);
+            follower.follow(leader);
+        }
+
+        return leader;
+    }
+}
