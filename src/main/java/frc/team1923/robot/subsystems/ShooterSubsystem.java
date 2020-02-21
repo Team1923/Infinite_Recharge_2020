a83736dac12adb9283da7e470e2e64be3329a678
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-02-21 17:01:12 -0500

    Add telemetry to Shooter

--- src/main/java/frc/team1923/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/team1923/robot/subsystems/ShooterSubsystem.java
@@ -3,6 +3,7 @@ package frc.team1923.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.team1923.robot.Constants.Shooter;
@@ -24,5 +25,10 @@ public class ShooterSubsystem extends SubsystemBase {
 
     public void stop() {
         this.shooter.stopMotor();
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Shooter Velocity", this.getVelocity());
     }
 }
