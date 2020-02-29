1480674c59b9551c265904d15143cb34d1208e75
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-02-29 11:45:04 -0500

    Reduce curve strength for drive

--- src/main/java/frc/team1923/robot/commands/drivetrain/DriveArcadeCommand.java
+++ src/main/java/frc/team1923/robot/commands/drivetrain/DriveArcadeCommand.java
@@ -12,8 +12,8 @@ public class DriveArcadeCommand extends DrivetrainCommand {
         double left = this.driver.leftStick.y.get();
         double right = this.driver.rightStick.x.get();
 
-        left = Math.pow(left, 3);
-        right = Math.pow(right, 3);
+        left *= Math.abs(left);
+        right *= Math.abs(right);
 
         this.drivetrain.set(left + right, left - right);
     }
--- src/main/java/frc/team1923/robot/commands/drivetrain/DriveTankCommand.java
+++ src/main/java/frc/team1923/robot/commands/drivetrain/DriveTankCommand.java
@@ -12,8 +12,8 @@ public class DriveTankCommand extends DrivetrainCommand {
         double left = this.driver.leftStick.y.get();
         double right = this.driver.rightStick.y.get();
 
-        left = Math.pow(left, 3);
-        right = Math.pow(right, 3);
+        left *= Math.abs(left);
+        right *= Math.abs(right);
 
         this.drivetrain.set(left, right);
     }
