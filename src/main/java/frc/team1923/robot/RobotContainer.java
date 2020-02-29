bf2936b13e99aff53aa4d07954700e1f973a7e03
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-02-29 12:19:22 -0500

    Update Container to latest subsystems and commands

--- src/main/java/frc/team1923/robot/RobotContainer.java
+++ src/main/java/frc/team1923/robot/RobotContainer.java
@@ -3,15 +3,17 @@ package frc.team1923.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.team1923.robot.Constants.Controllers;
+import frc.team1923.robot.commands.conveyor.ConveyorControlCommand;
+import frc.team1923.robot.commands.conveyor.ConveyorShiftCommand;
 import frc.team1923.robot.commands.drivetrain.DriveArcadeCommand;
-import frc.team1923.robot.commands.indexer.IndexerIndexCommand;
-import frc.team1923.robot.commands.indexer.IndexerShiftCommand;
-import frc.team1923.robot.commands.intake.IntakeCycleCommand;
-import frc.team1923.robot.commands.intake.IntakeIntakeCommand;
+import frc.team1923.robot.commands.indexer.IndexerControlCommand;
+import frc.team1923.robot.commands.indexer.IndexerCycleCommand;
+import frc.team1923.robot.commands.intake.IntakeControlCommand;
 import frc.team1923.robot.commands.shooter.ShooterShootCommand;
 import frc.team1923.robot.commands.turret.TurretFollowCommand;
-import frc.team1923.robot.commands.turret.TurretRotateCommand;
+import frc.team1923.robot.commands.turret.TurretControlCommand;
 import frc.team1923.robot.subsystems.ControlPanelSubsystem;
+import frc.team1923.robot.subsystems.ConveyorSubsystem;
 import frc.team1923.robot.subsystems.ClimberSubsystem;
 import frc.team1923.robot.subsystems.DrivetrainSubsystem;
 import frc.team1923.robot.subsystems.IndexerSubsystem;
@@ -19,6 +21,7 @@ import frc.team1923.robot.subsystems.IntakeSubsystem;
 import frc.team1923.robot.subsystems.ShooterSubsystem;
 import frc.team1923.robot.subsystems.TurretSubsystem;
 import frc.team1923.robot.utilities.Limelight;
+import frc.team1923.robot.utilities.command.RepeatedCommand;
 import frc.team1923.robot.utilities.controller.PS4Controller;
 import frc.team1923.robot.utilities.controller.XboxController;
 
@@ -27,6 +30,7 @@ public class RobotContainer {
     public final ShooterSubsystem shooter = new ShooterSubsystem();
     public final IntakeSubsystem intake = new IntakeSubsystem();
     public final IndexerSubsystem indexer = new IndexerSubsystem();
+    public final ConveyorSubsystem conveyor = new ConveyorSubsystem();
     public final TurretSubsystem turret = new TurretSubsystem();
     public final ControlPanelSubsystem controlPanel = null; //new ControlPanelSubsystem();
     public final ClimberSubsystem climber = null; //new ClimberSubsystem();
@@ -38,16 +42,15 @@ public class RobotContainer {
 
     public RobotContainer() {
         new DriveArcadeCommand(this).setAsDefault();
-        new IntakeIntakeCommand(this).setAsDefault();
-        new IndexerIndexCommand(this).setAsDefault();
-        new TurretRotateCommand(this).setAsDefault();
+        new IntakeControlCommand(this).setAsDefault();
+        new IndexerControlCommand(this).setAsDefault();
+        new ConveyorControlCommand(this).setAsDefault();
+        new TurretControlCommand(this).setAsDefault();
 
-        this.operator.b.whenHeld(new TurretFollowCommand(this));
-        this.operator.a.whenHeld(new IndexerShiftCommand(this));
-
-        this.operator.dPad.down.whenPressed(new IntakeCycleCommand(this));
-
-        this.operator.x.toggleWhenPressed(new ShooterShootCommand(this, 0.85));
+        this.operator.x.toggleWhenPressed(new ShooterShootCommand(this, 5000));
+        this.operator.dPad.down.whenPressed(new IndexerCycleCommand(this));
+        this.operator.a.toggleWhenPressed(new RepeatedCommand(new ConveyorShiftCommand(this)));
+        this.operator.b.toggleWhenPressed(new TurretFollowCommand(this));
     }
 
     public Command getAutonomousCommand() {
