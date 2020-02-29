7e6234b63411089474cebe6fc1c092c0215535fa
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-02-29 10:28:51 -0500

    Add ConfigurableDouble

---
+++ src/main/java/frc/team1923/robot/utilities/ConfigurableDouble.java
@@ -0,0 +1,21 @@
+package frc.team1923.robot.utilities;
+
+import edu.wpi.first.networktables.EntryListenerFlags;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.util.function.DoubleConsumer;
+
+public class ConfigurableDouble {
+    public ConfigurableDouble(String name, DoubleConsumer callback, double defaultValue) {
+        NetworkTableEntry entry = SmartDashboard.getEntry(name);
+        entry.addListener(entryNotification -> callback.accept(entryNotification.value.getDouble()), EntryListenerFlags.kUpdate);
+
+        entry.setDefaultDouble(defaultValue);
+        callback.accept(entry.getDouble(defaultValue));
+    }
+
+    public ConfigurableDouble(String name, DoubleConsumer callback) {
+        this(name, callback, 0);
+    }
+}
