b386ce04590d0897863ee0349f532392887561ce
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-01 10:44:50 -0500

    add open loop ramping

--- src/main/java/frc/team1923/robot/utilities/motor/SparkMaxGroup.java
+++ src/main/java/frc/team1923/robot/utilities/motor/SparkMaxGroup.java
@@ -17,7 +17,9 @@ public class SparkMaxGroup extends MotorGroup<SparkMaxGroup> {
         leader.restoreFactoryDefaults();
         leader.setInverted(this.inverted);
         leader.setIdleMode(this.coast ? IdleMode.kCoast : IdleMode.kBrake);
+
         leader.setClosedLoopRampRate(this.rampRate);
+        leader.setOpenLoopRampRate(this.rampRate);
 
         CANPIDController pidController = leader.getPIDController();
         pidController.setP(this.p);
