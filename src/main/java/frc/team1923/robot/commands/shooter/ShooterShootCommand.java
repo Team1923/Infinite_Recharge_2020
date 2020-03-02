a514aa493bfb58378498b0b601a8f62bef4cdad8
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-02 01:11:48 -0500

    Add distance tracking and RPM calculation

--- src/main/java/frc/team1923/robot/commands/shooter/ShooterShootCommand.java
+++ src/main/java/frc/team1923/robot/commands/shooter/ShooterShootCommand.java
@@ -5,25 +5,37 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team1923.robot.RobotContainer;
 
 public class ShooterShootCommand extends ShooterCommand {
-    private double velocity;
-
-    public ShooterShootCommand(RobotContainer robotContainer, double velocity) {
+    public ShooterShootCommand(RobotContainer robotContainer) {
         super(robotContainer);
 
-        this.velocity = velocity;
+        SmartDashboard.putNumber("Target RPM", -1337);
+        SmartDashboard.putNumber("Target RPM", 0);
 
-        SmartDashboard.putNumber("Target RPM", velocity);
-    }
-
-    @Override
-    public void initialize() {
-        this.velocity = SmartDashboard.getNumber("Target RPM", this.velocity);
-        this.shooter.setVelocity(this.velocity);
+        SmartDashboard.putBoolean("Auto Adjust RPM", false);
+        SmartDashboard.putBoolean("Auto Adjust RPM", true);
     }
 
     @Override
     public void execute() {
-        SmartDashboard.putBoolean("Spun Up", this.shooter.getVelocity() > this.velocity - 50);
+        double velocity;
+
+        if (SmartDashboard.getBoolean("Auto Adjust RPM", true)) {
+            final double LIMELIGHT_ANGLE = 23;
+            final double LIMELIGHT_HEIGHT = 42;
+            final double TARGET_HEIGHT = 98.25;
+
+            double distance = (TARGET_HEIGHT - LIMELIGHT_HEIGHT) / Math.tan(Math.toRadians(this.limelight.getY() + LIMELIGHT_ANGLE));
+
+            velocity = distance * 5.2907 + 2819.77;
+
+            SmartDashboard.putNumber("Target RPM", velocity);
+        } else {
+            velocity = SmartDashboard.getNumber("Target RPM", 0);
+        }
+
+        this.shooter.setVelocity(velocity);
+
+        SmartDashboard.putBoolean("Spun Up", this.shooter.getVelocity() > velocity - 50);
     }
 
     @Override
