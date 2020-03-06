e210af3247ff782f276fe2fdc659c1e1a655b317
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-06 16:48:15 -0500

    Update Indexer Cycle timings

--- src/main/java/frc/team1923/robot/commands/indexer/IndexerCycleCommand.java
+++ src/main/java/frc/team1923/robot/commands/indexer/IndexerCycleCommand.java
@@ -1,6 +1,7 @@
 package frc.team1923.robot.commands.indexer;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import frc.team1923.robot.RobotContainer;
 
@@ -8,7 +9,9 @@ public class IndexerCycleCommand extends SequentialCommandGroup {
     public IndexerCycleCommand(RobotContainer robotContainer) {
         this.addCommands(
             new IndexerSetCommand(robotContainer, -0.75).withTimeout(0.25),
-            new IndexerSetCommand(robotContainer, 0.75).withTimeout(0.25)
+            new WaitCommand(0.25),
+            new IndexerSetCommand(robotContainer, 0.75).withTimeout(0.25),
+            new WaitCommand(0.25)
         );
     }
 }
