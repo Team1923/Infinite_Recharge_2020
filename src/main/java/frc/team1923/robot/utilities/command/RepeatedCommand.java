ae0bb362887ab01f79ca98bf6922565dec14632c
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-07 16:44:17 -0500

    Rework Repeated Command

--- src/main/java/frc/team1923/robot/utilities/command/RepeatedCommand.java
+++ src/main/java/frc/team1923/robot/utilities/command/RepeatedCommand.java
@@ -1,38 +1,20 @@
 package frc.team1923.robot.utilities.command;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.PerpetualCommand;
 
-public class RepeatedCommand extends CommandBase {
-    private final Command command;
-
+public class RepeatedCommand extends PerpetualCommand {
     public RepeatedCommand(Command command) {
-        this.command = command;
-        this.m_requirements.addAll(command.getRequirements());
-    }
-
-    @Override
-    public void initialize() {
-        this.command.initialize();
+        super(command);
     }
 
     @Override
     public void execute() {
-        this.command.execute();
+        super.execute();
 
-        if (this.command.isFinished()) {
-            this.command.end(false);
-            this.command.initialize();
+        if (this.m_command.isFinished()) {
+            this.end(false);
+            this.initialize();
         }
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        this.command.end(interrupted);
-    }
-
-    @Override
-    public boolean runsWhenDisabled() {
-        return this.command.runsWhenDisabled();
     }
 }
