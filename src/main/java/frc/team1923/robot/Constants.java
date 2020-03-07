fad387130b476063c7ffeeb2ac4694abe1eae825
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-07 17:02:06 -0500

    Compute turret soft limits

--- src/main/java/frc/team1923/robot/Constants.java
+++ src/main/java/frc/team1923/robot/Constants.java
@@ -44,7 +44,9 @@ public final class Constants {
     public static final class Turret {
         private Turret() {}
 
-        public static final SparkMaxGroup TURRET = new SparkMaxGroup(10).softLimiting(186, -33).withPIDF(0.04, 0, 0, 0);
+        public static final double TURRET_RATIO = 27 * 120 / 22.0;
+
+        public static final SparkMaxGroup TURRET = new SparkMaxGroup(10).softLimiting(TURRET_RATIO * 5 / 4, -TURRET_RATIO / 4).withPIDF(0.04, 0, 0, 0);
 
         public static final int IS_AT_ZERO_SENSOR = 3;
     }
