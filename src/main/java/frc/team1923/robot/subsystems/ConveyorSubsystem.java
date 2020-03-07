8fa8a95b0f372359c47cac403384f72ac00ad440
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-07 16:58:31 -0500

    Formatting changes

--- src/main/java/frc/team1923/robot/subsystems/ConveyorSubsystem.java
+++ src/main/java/frc/team1923/robot/subsystems/ConveyorSubsystem.java
@@ -10,8 +10,8 @@ import frc.team1923.robot.Constants.Conveyor;
 public class ConveyorSubsystem extends SubsystemBase {
     private CANSparkMax conveyor = Conveyor.CONVEYOR.create();
 
-    private DigitalInput conveyorHasBallSensor = new DigitalInput(Conveyor.CONVEYOR_HAS_BALL_SENSOR);
     private DigitalInput indexerHasBallSensor = new DigitalInput(Conveyor.INDEXER_HAS_BALL_SENSOR);
+    private DigitalInput conveyorHasBallSensor = new DigitalInput(Conveyor.CONVEYOR_HAS_BALL_SENSOR);
     private DigitalInput turretHasBallSensor = new DigitalInput(Conveyor.TURRET_HAS_BALL_SENSOR);
 
     public void set(double speed) {
@@ -22,12 +22,12 @@ public class ConveyorSubsystem extends SubsystemBase {
         this.conveyor.stopMotor();
     }
 
-    public boolean conveyorHasBall() {
-        return !this.conveyorHasBallSensor.get();
-    }
-
     public boolean indexerHasBall() {
         return !this.indexerHasBallSensor.get();
+    }
+
+    public boolean conveyorHasBall() {
+        return !this.conveyorHasBallSensor.get();
     }
 
     public boolean turretHasBall() {
