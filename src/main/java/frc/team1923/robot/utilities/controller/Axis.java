2c0283e8286f51b0b81f01dd50c202c8c8d6f101
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-08 19:39:44 -0400

    Rework Controller api

---
+++ src/main/java/frc/team1923/robot/utilities/controller/Axis.java
@@ -0,0 +1,64 @@
+package frc.team1923.robot.utilities.controller;
+
+import edu.wpi.first.wpilibj2.command.button.Button;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.DoubleUnaryOperator;
+
+public class Axis {
+    private final DoubleSupplier value;
+
+    public Axis(DoubleSupplier value) {
+        this.value = value;
+    }
+
+    public Axis() {
+        this(() -> 0);
+    }
+
+    public double get() {
+        return this.value.getAsDouble();
+    }
+
+    public Button withLowerTreshold(double lower) {
+        return new Button(() -> this.get() >= lower);
+    }
+
+    public Button withUpperTreshold(double upper) {
+        return new Button(() -> this.get() <= upper);
+    }
+
+    public static Axis fromButton(Button button, double falseValue, double trueValue) {
+        return new Axis(() -> button.get() ? trueValue : falseValue);
+    }
+
+    public static Axis fromButton(Button button) {
+        return Axis.fromButton(button, 0, 1);
+    }
+
+    public Axis withDeadzone(double deadzone) {
+        return new Axis(() -> {
+            double value = this.get();
+            return Math.copySign(Math.max(0, (Math.abs(value) - deadzone) / (1 - deadzone)), value);
+        });
+    }
+
+    public Axis invert() {
+        return new Axis(() -> -this.get());
+    }
+
+    public Axis positive() {
+        return new Axis(() -> this.get() * 0.5 + 0.5);
+    }
+
+    public Axis squared() {
+        return new Axis(() -> {
+            double value = this.get();
+            return value * Math.abs(value);
+        });
+    }
+
+    public Axis map(DoubleUnaryOperator function) {
+        return new Axis(() -> function.applyAsDouble(this.get()));
+    }
+}
--- src/main/java/frc/team1923/robot/utilities/controller/Controller.java
+++ src/main/java/frc/team1923/robot/utilities/controller/Controller.java
@@ -4,129 +4,36 @@ import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.button.Button;
 
-import java.util.Arrays;
-
 public abstract class Controller {
     private final int port;
 
-    protected enum AxisRange {
-        Normal, Inverted, Positive
-    }
-
-    public class Axis {
-        private final int axis;
-        private final AxisRange range;
-        private final double deadzone;
-
-        protected Axis(int axis, AxisRange range, double deadzone) {
-            this.axis = axis;
-            this.range = range;
-            this.deadzone = deadzone;
-        }
-
-        protected Axis(int axis, AxisRange range) {
-            this(axis, range, 0);
-        }
-
-        protected Axis(int axis, double deadzone) {
-            this(axis, AxisRange.Normal, deadzone);
-        }
-
-        protected Axis(int axis) {
-            this(axis, AxisRange.Normal, 0);
-        }
-
-        public double get() {
-            double raw = DriverStation.getInstance().getStickAxis(Controller.this.port, this.axis);
-
-            switch (this.range) {
-                case Inverted:
-                    raw = -raw;
-                    break;
-                case Positive:
-                    raw = 0.5 * raw + 0.5;
-                    break;
-                default:
-            }
-
-            return Math.copySign(Math.max(0, (Math.abs(raw) - this.deadzone) / (1 - this.deadzone)), raw);
-        }
-
-        public Button withLowerTreshold(double lower) {
-            return new Button(() -> this.get() >= lower);
-        }
-
-        public Button withUpperTreshold(double upper) {
-            return new Button(() -> this.get() <= upper);
-        }
-    }
-
-    public class Joystick {
-        public final Axis x, y;
-
-        protected Joystick(int xAxis, int yAxis, AxisRange xRange, AxisRange yRange, double deadzone) {
-            this.x = new Axis(xAxis, xRange, deadzone);
-            this.y = new Axis(yAxis, yRange, deadzone);
-        }
-
-        protected Joystick(int xAxis, int yAxis, AxisRange xRange, AxisRange yRange) {
-            this.x = new Axis(xAxis, xRange);
-            this.y = new Axis(yAxis, yRange);
-        }
-
-        protected Joystick(int xAxis, int yAxis, double deadzone) {
-            this.x = new Axis(xAxis, deadzone);
-            this.y = new Axis(yAxis, deadzone);
-        }
-
-        protected Joystick(int xAxis, int yAxis) {
-            this.x = new Axis(xAxis);
-            this.y = new Axis(yAxis);
-        }
-    }
-
-    protected class ControllerButton extends Button {
-        private final int button;
-
-        protected ControllerButton(int button) {
-            this.button = button;
-        }
-
-        @Override
-        public boolean get() {
-            return DriverStation.getInstance().getStickButton(Controller.this.port, this.button);
-        }
-    }
-
-    public class DPad {
-        private final int pov;
-
-        private class DPadButton extends Button {
-            private final double[] angles;
-
-            private DPadButton(double... angles) {
-                this.angles = angles;
-            }
-
-            @Override
-            public boolean get() {
-                double angle = DriverStation.getInstance().getStickPOV(Controller.this.port, DPad.this.pov);
-                return Arrays.stream(this.angles).anyMatch(i -> i == angle);
-            }
-        }
-
-        public final Button left = new DPadButton(225, 270, 315);
-        public final Button up = new DPadButton(315, 0, 45);
-        public final Button right = new DPadButton(45, 90, 135);
-        public final Button down = new DPadButton(135, 180, 225);
-
-        protected DPad(int pov) {
-            this.pov = pov;
-        }
-    }
-
     protected Controller(int port) {
         this.port = port;
+    }
+
+    protected Axis axis(int axis) {
+        return new Axis(() -> DriverStation.getInstance().getStickAxis(this.port, axis));
+    }
+
+    protected Button button(int button) {
+        return new Button(() -> DriverStation.getInstance().getStickButton(this.port, button));
+    }
+
+    protected DPad dPad(int pov) {
+        return new DPad(
+            this.povButton(pov, 225, 270, 315),
+            this.povButton(pov, 315, 0, 45),
+            this.povButton(pov, 45, 90, 135),
+            this.povButton(pov, 135, 180, 225)
+        );
+    }
+
+    private Button povButton(int pov, double... angles) {
+        return new Button(() -> {
+            double angle = DriverStation.getInstance().getStickPOV(this.port, pov);
+            for (double i : angles) if (i == angle) return true;
+            return false;
+        });
     }
 
     private short leftRumble, rightRumble;
