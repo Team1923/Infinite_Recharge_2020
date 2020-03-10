b0e24d9517b3f42f66ecdfef60d823018af44264
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-10 01:16:04 -0400

    Rework indexer commands

--- src/main/java/frc/team1923/robot/commands/indexer/IndexerCycleCommand.java
+++ src/main/java/frc/team1923/robot/commands/indexer/IndexerCycleCommand.java
@@ -2,15 +2,16 @@ package frc.team1923.robot.commands.indexer;
 
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-import frc.team1923.robot.RobotContainer;
-import frc.team1923.robot.commands.CommandGroup;
+import frc.team1923.robot.utilities.command.CommandGroup;
+import frc.team1923.robot.utilities.command.SetCommand;
+import frc.team1923.robot.utilities.command.Subsystem;
 
 public class IndexerCycleCommand extends CommandGroup {
-    public IndexerCycleCommand(RobotContainer robotContainer) {
+    public IndexerCycleCommand(Subsystem indexer) {
         this.addCommands(
-            new IndexerSetCommand(robotContainer, -0.75).withTimeout(0.25),
+            new SetCommand(indexer, -0.75).withTimeout(0.25),
             new WaitCommand(0.1),
-            new IndexerSetCommand(robotContainer, 0.75).withTimeout(0.25),
+            new SetCommand(indexer, 0.75).withTimeout(0.25),
             new WaitCommand(0.1)
         );
     }
