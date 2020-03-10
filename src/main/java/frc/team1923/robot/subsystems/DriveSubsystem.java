1096e906062f479ed53bc18cf593c44c184fef98
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-10 01:12:55 -0400

    Optimize Drive motors

--- src/main/java/frc/team1923/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/team1923/robot/subsystems/DriveSubsystem.java
@@ -4,10 +4,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.team1923.robot.Constants.Drive;
 import frc.team1923.robot.utilities.motor.Motor;
+import frc.team1923.robot.utilities.motor.OptimizedMotor;
 
 public class DriveSubsystem extends SubsystemBase {
-    private final Motor left = Drive.LEFT.create();
-    private final Motor right = Drive.RIGHT.create();
+    private final Motor left = new OptimizedMotor(Drive.LEFT.create());
+    private final Motor right = new OptimizedMotor(Drive.RIGHT.create());
 
     public void setSpeed(double left, double right) {
         this.left.setSpeed(left);
