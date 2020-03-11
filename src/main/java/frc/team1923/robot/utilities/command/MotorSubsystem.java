f081117b1a78f3133eb93029c192a2acfb60515b
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-03-11 14:07:56 -0400

    Simplify motor subsystem api

--- src/main/java/frc/team1923/robot/utilities/command/MotorSubsystem.java
+++ src/main/java/frc/team1923/robot/utilities/command/MotorSubsystem.java
@@ -2,11 +2,19 @@ package frc.team1923.robot.utilities.command;
 
 import frc.team1923.robot.utilities.motor.Motor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MotorSubsystem extends Subsystem {
-    private final Motor[] motors;
+    private List<Motor> motors;
 
     public MotorSubsystem(Motor... motors) {
-        this.motors = motors;
+        this.motors = new ArrayList<>(Arrays.asList(motors));
+    }
+
+    protected void add(Motor... motors) {
+        this.motors.addAll(Arrays.asList(motors));
     }
 
     @Override
--- src/main/java/frc/team1923/robot/utilities/command/SingleMotorSubsystem.java
+++ src/main/java/frc/team1923/robot/utilities/command/SingleMotorSubsystem.java
@@ -9,26 +9,11 @@ public class SingleMotorSubsystem<T extends Motor> extends MotorSubsystem {
     private final T rawMotor;
     private final OptimizedMotor motor;
 
-    private static Motor tempRawMotor;
-    private static OptimizedMotor tempMotor;
-
-    private static Motor wrapGroup(MotorGroup<?> motorGroup) {
-        SingleMotorSubsystem.tempRawMotor = motorGroup.create();
-        SingleMotorSubsystem.tempMotor = new OptimizedMotor(SingleMotorSubsystem.tempRawMotor);
-        return SingleMotorSubsystem.tempMotor;
-    }
-
     public SingleMotorSubsystem(MotorGroup<T> motorGroup) {
-        super(SingleMotorSubsystem.wrapGroup(motorGroup));
+        this.rawMotor = motorGroup.create();
+        this.motor = new OptimizedMotor(this.rawMotor);
 
-        @SuppressWarnings("unchecked")
-        T rawMotor = (T) SingleMotorSubsystem.tempRawMotor;
-
-        this.rawMotor = rawMotor;
-        this.motor = SingleMotorSubsystem.tempMotor;
-
-        SingleMotorSubsystem.tempRawMotor = null;
-        SingleMotorSubsystem.tempMotor = null;
+        this.add(this.motor);
     }
 
     protected T getMotor() {
