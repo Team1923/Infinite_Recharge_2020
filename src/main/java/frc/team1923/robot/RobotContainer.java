773aa641706d9c04f3664cb80e015cb685fabd35
aryanjain200425 <46764031+aryanjain200425@users.noreply.github.com>
2021-11-03 20:35:51 -0400

    Add files via upload
    Removed sensor code

--- src/main/java/frc/team1923/robot/RobotContainer.java
+++ src/main/java/frc/team1923/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.team1923.robot.Constants.Controllers;
 import frc.team1923.robot.commands.AutoIndexCommand;
 import frc.team1923.robot.commands.drive.DriveArcadeCommand;
+import frc.team1923.robot.commands.indexer.IndexerCycleCommand;
 import frc.team1923.robot.commands.intake.IntakeCycleCommand;
 import frc.team1923.robot.commands.intake.IntakeIntakeCommand;
 import frc.team1923.robot.commands.shooter.ShooterShootCommand;
@@ -25,6 +26,7 @@ import frc.team1923.robot.utilities.controller.Axis;
 import frc.team1923.robot.utilities.controller.PS4Controller;
 import frc.team1923.robot.utilities.controller.XboxController;
 import frc.team1923.robot.utilities.dashboard.ConfigurableDouble;
+import frc.team1923.robot.utilities.vision.Limelight;
 
 public class RobotContainer {
     public final DriveSubsystem drive = new DriveSubsystem();
@@ -34,8 +36,12 @@ public class RobotContainer {
     public final ConveyorSubsystem conveyor = new ConveyorSubsystem();
     public final TurretSubsystem turret = new TurretSubsystem();
     public final ClimberSubsystem climber = new ClimberSubsystem();
+  
 
     public final TurretLimelight limelight = new TurretLimelight();
+    public final Limelight new_limeLight = new Limelight();
+    public final IndexerCycleCommand indexercyclecommand = new IndexerCycleCommand(indexer);
+    public final ShooterShootCommand shooterCommand = new ShooterShootCommand(shooter);
 
     public final PS4Controller driver = new PS4Controller(Controllers.DRIVER);
     public final XboxController operator = new XboxController(Controllers.OPERATOR);
@@ -45,37 +51,53 @@ public class RobotContainer {
     private double speedValue = 0.8;
 
     public RobotContainer() {
+
+        this.limelight.enable();
+        double distance = limelight.getDistance();
+
+        System.out.println(distance);
+
         new DriveArcadeCommand(this.drive, this.driver.leftStick.y.squared(), this.driver.rightStick.x.squared()).setAsDefault();
         this.intake.control(this.operator.leftStick.y).setAsDefault();
         this.conveyor.control(this.operator.rightStick.y).setAsDefault();
         this.turret.control(this.operator.leftTrigger.subtract(this.operator.rightTrigger)).setAsDefault(); // or gohomecommand.andthen control
+        
+        // AutoIndexCommand cmd = new AutoIndexCommand(this);
+        // this.operator.a.toggleWhenPressed(
+        //     new RepeatedCommand(cmd)
+        //     .withInterrupt(() -> cmd.balls == 5)
+        //     .deadlineWith(new IntakeInpdtakeCommand(this.intake, this.operator.leftStick.y, new IntakeCycleCommand(this.intake)))
+        // );
 
-        AutoIndexCommand cmd = new AutoIndexCommand(this);
-        this.operator.a.toggleWhenPressed(
-            new RepeatedCommand(cmd)
-            .withInterrupt(() -> cmd.balls == 5)
-            .deadlineWith(new IntakeIntakeCommand(this.intake, this.operator.leftStick.y, new IntakeCycleCommand(this.intake).repeatedly()))
-        );
-        Command[] b = {
-            new ShooterShootCommand(this.shooter),
-            new FunctionalCommand(() -> {}, () -> {}, interrupted -> cmd.balls = 0, () -> false),
-            new TurretFollowCommand(this.turret, this.limelight)
-        };
+        this.operator.a.toggleWhenPressed(indexercyclecommand);
+        
+        // Command[] b = {
+        //     new ShooterShootCommand(this.shooter),
+        //     new FunctionalCommand(() -> {}, () -> {}, interrupted -> cmd.balls = 0, () -> false),
+        //     new TurretFollowCommand(this.turret, this.limelight)
+        // };
 
-        for (Command c : b) {
-            this.operator.b.toggleWhenPressed(c);
-        }
+        // for (Command c : b) {
+        //     this.operator.b.toggleWhenPressed(c);
+        // }
 
-        this.operator.x.toggleWhenPressed((new WaitUntilCommand(() -> this.shooter.spunUp() && this.limelight.hasValidTarget()).raceWith(new WaitCommand(2))).andThen(() -> this.timeSinceLastBall = Timer.getFPGATimestamp()).andThen(this.conveyor.set(this.speedValue).withInterrupt(() -> {
-            if (this.conveyor.turretHasBall()) this.timeSinceLastBall = Timer.getFPGATimestamp();
-            return Timer.getFPGATimestamp() - this.timeSinceLastBall > 0.75;
-        })).andThen(() -> {
-            for (Command c : b) {
-                if (c.isScheduled()) {
-                    c.cancel();
-                }
-            }
-        }));
+
+        //need to verify if this auto-adjustment works (based on the calculation put directly into the controller command)
+       
+
+        
+            this.operator.x.toggleWhenPressed(shooterCommand);
+
+        // this.operator.x.toggleWhenPressed((new WaitUntilCommand(() -> this.shooter.spunUp() && this.limelight.hasValidTarget()).raceWith(new WaitCommand(2))).andThen(() -> this.timeSinceLastBall = Timer.getFPGATimestamp()).andThen(this.conveyor.set(this.speedValue).withInterrupt(() -> {
+        //     if (this.conveyor.turretHasBall()) this.timeSinceLastBall = Timer.getFPGATimestamp();
+        //     return Timer.getFPGATimestamp() - this.timeSinceLastBall > 0.75;
+        // })).andThen(() -> {
+        //     // for (Command c : b) {
+        //     //     if (c.isScheduled()) {
+        //     //         c.cancel();
+        //     //     }
+        //     // }
+        // }));
 
         this.operator.menu.whenPressed(() -> this.intake.bringDown(), this.intake);
         this.operator.view.whenPressed(() -> this.intake.bringUp(), this.intake);
@@ -83,10 +105,10 @@ public class RobotContainer {
         this.driver.dPad.left.whenPressed(() -> this.climber.bringDown(), this.climber);
         this.driver.dPad.right.whenPressed(() -> this.climber.bringUp(), this.climber);
 
-        //this.climber.control(Axis.fromButton(this.driver.dPad.up).subtract(Axis.fromButton(this.driver.dPad.down))).setAsDefault();
+        this.climber.control(Axis.fromButton(this.driver.dPad.down).subtract(Axis.fromButton(this.driver.dPad.up))).setAsDefault();
 
-        this.driver.dPad.up.whenHeld(this.climber.set(1));
         this.driver.dPad.up.whenHeld(this.climber.set(-1));
+        this.driver.dPad.down.whenHeld(this.climber.set(1));
 
         this.operator.y.toggleWhenPressed(new ShooterShootCommand(this.shooter));
 
