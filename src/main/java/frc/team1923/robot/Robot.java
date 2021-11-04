38d2df2eb6eba74e54afb29f43d726d20d33d690
aryanjain200425 <46764031+aryanjain200425@users.noreply.github.com>
2021-11-03 20:36:49 -0400

    Add files via upload
    Added (Currently not working) Auton Code

--- src/main/java/frc/team1923/robot/Robot.java
+++ src/main/java/frc/team1923/robot/Robot.java
@@ -1,12 +1,28 @@
 package frc.team1923.robot;
 
+import java.security.KeyStore.PrivateKeyEntry;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
+import frc.team1923.robot.subsystems.ConveyorSubsystem;
+import frc.team1923.robot.subsystems.DriveSubsystem;
+import frc.team1923.robot.subsystems.IntakeSubsystem;
+import frc.team1923.robot.subsystems.ShooterSubsystem;
+import jdk.dynalink.linker.ConversionComparator;
 
 public class Robot extends TimedRobot {
     private RobotContainer robotContainer;
+
+    // private DriveSubsystem drivetrain;
+    // private ShooterSubsystem shooter;
+    // private ConveyorSubsystem conveyor;
+    // private IntakeSubsystem intake;
+    private double startTime;
 
     @Override
     public void robotInit() {
@@ -17,11 +33,41 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        Command autonomousCommand = this.robotContainer.getAutonomousCommand();
 
-        if (autonomousCommand != null) {
-            autonomousCommand.schedule();
-        }
+        robotContainer.intake.bringDown();
+
+        robotContainer.drive.setSpeed(0.5, 0.5);
+
+
+
+
+        // Command autonomousCommand = this.robotContainer.getAutonomousCommand();
+
+        // if (autonomousCommand != null) {
+        //     autonomousCommand.schedule();
+        // }
+    }
+
+
+    public void autonomousPeriodic() {
+
+        //Scheduler.getInstance().run();
+
+
+        // double timer = Timer.getFPGATimestamp();
+
+        // robotContainer.shooter.set(5500);
+
+        // if(timer - startTime == 3){
+        //     robotContainer.conveyor.set(4000);
+        // }
+
+       
+            
+     
+
+
+
     }
 
     @Override
